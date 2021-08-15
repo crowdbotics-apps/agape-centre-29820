416b85439737409786cc33c4039e245a38aa7346
Crowdbotics <team@crowdbotics.com>
2021-08-15 01:32:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "agape_centre_29820Identifier",
+  url: "https://agape_centre_29820Identifier.botics.co",
+  api: "https://agape_centre_29820Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
