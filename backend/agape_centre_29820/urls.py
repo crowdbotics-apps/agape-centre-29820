9b97aab1ce4aac28cf762aa7089ce3109c3c565b
Crowdbotics <team@crowdbotics.com>
2021-08-15 01:33:26 +0000

    Update admin title

--- backend/agape_centre_29820/urls.py
+++ backend/agape_centre_29820/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Agape Centre"
-admin.site.site_title = "Agape Centre Admin Portal"
-admin.site.index_title = "Agape Centre Admin"
+admin.site.site_header = "KP3"
+admin.site.site_title = "KP3 Admin Portal"
+admin.site.index_title = "KP3 Admin"
 
 # swagger
 api_info = openapi.Info(
